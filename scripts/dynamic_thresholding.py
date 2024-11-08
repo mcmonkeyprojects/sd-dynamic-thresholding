0270e030fe7a1e1d12acd256ffde901055461d48
Haoming <hmstudy02@gmail.com>
2024-11-08 12:20:53 +0800

    remove outdated codes

--- scripts/dynamic_thresholding.py
+++ scripts/dynamic_thresholding.py
@@ -45,8 +45,6 @@ class Script(scripts.Script):
         def vis_change(is_vis):
             return {"visible": is_vis, "__type__": "update"}
         # "Dynamic Thresholding (CFG Scale Fix)"
-        dtrue = gr.Checkbox(value=True, visible=False)
-        dfalse = gr.Checkbox(value=False, visible=False)
         with InputAccordion(label="Dynamic Thresholding (CFG Scale Fix)", value=False, elem_id="dynthres_" + ("img2img" if is_img2img else "txt2img")) as enabled:
             with gr.Group():
                 gr.HTML(value=f"View <a style=\"border-bottom: 1px #00ffff dotted;\" href=\"https://github.com/mcmonkeyprojects/sd-dynamic-thresholding/wiki/Usage-Tips\">the wiki for usage tips.</a><br><br>", elem_id='dynthres_wiki_link')
@@ -70,13 +68,8 @@ class Script(scripts.Script):
             return vis_change(sched_vis), vis_change(mimic_mode != "Constant" or DISABLE_VISIBILITY), vis_change(cfg_mode != "Constant" or DISABLE_VISIBILITY)
         cfg_mode.change(should_show_scheduler_value, inputs=[cfg_mode, mimic_mode], outputs=[sched_val, mimic_scale_min, cfg_scale_min])
         mimic_mode.change(should_show_scheduler_value, inputs=[cfg_mode, mimic_mode], outputs=[sched_val, mimic_scale_min, cfg_scale_min])
-        enabled.change(
-            _js="dynthres_update_enabled",
-            fn=None,
-            inputs=[enabled, dtrue if is_img2img else dfalse],
-            show_progress = False)
         self.infotext_fields = (
-            (enabled, lambda d: gr.Checkbox.update(value="Dynamic thresholding enabled" in d)),
+            (enabled, lambda d: gr.update(value="Dynamic thresholding enabled" in d)),
             (mimic_scale, "Mimic scale"),
             (separate_feature_channels, "Separate Feature Channels"),
             (scaling_startpoint, lambda d: gr.Radio.update(value=d.get("Scaling Startpoint", "MEAN"))),
